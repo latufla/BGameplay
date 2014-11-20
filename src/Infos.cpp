78cdb79141f51cd7b5d6890c095b463599d41cfd
latufla <lafufla.work@gmail.com>
2014-11-20 16:39:11 +0600

    Infos refactor

--- src/Infos.cpp
+++ src/Infos.cpp
@@ -5,7 +5,10 @@ using std::unordered_map;
 using std::shared_ptr;
 using std::string;
 
-Infos::Infos(std::shared_ptr<FieldInfo> fieldInfo, unordered_map<string, shared_ptr<ObjectInfo>> nameToObjectInfo) {
-	this->fieldInfo = fieldInfo;
-	this->nameToObjectInfo = nameToObjectInfo;
+Infos::Infos(std::shared_ptr<FieldInfo> fieldInfo, unordered_map<string, shared_ptr<ObjectInfo>> nameToObjectInfo) 
+	: fieldInfo(fieldInfo), nameToObjectInfo(nameToObjectInfo){
+}
+
+std::weak_ptr<ObjectInfo> Infos::getObjectInfoBy(string name) {
+	return nameToObjectInfo.at(name);
 }
--- src/Infos.h
+++ src/Infos.h
@@ -6,11 +6,11 @@
 
 class Infos {
 public:
-	Infos(std::shared_ptr<FieldInfo>, std::unordered_map<std::string, std::shared_ptr<ObjectInfo>>);
+	Infos(std::shared_ptr<FieldInfo> fieldInfo, std::unordered_map<std::string, std::shared_ptr<ObjectInfo>> nameToObjectInfo);
 	~Infos() = default;
 
 	std::weak_ptr<FieldInfo> getFieldInfo() { return fieldInfo; }
-	std::weak_ptr<ObjectInfo> getObjectInfoBy(std::string name){ return nameToObjectInfo.at(name);}
+	std::weak_ptr<ObjectInfo> getObjectInfoBy(std::string name);
 	
 protected:
 	std::shared_ptr<FieldInfo> fieldInfo;
